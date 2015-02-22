e27eefb7e22ad743c3316f8e7fc0486f3d55ca3b
Jonathan BAUDIN <jjbaudin@gmail.com>
2015-02-22 01:18:15 +0100

    Fin du TP 3/4 sauf question 2.3

--- simul.c
+++ simul.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 // *** Compteur d'instruction ***
 int cptInstrution = 0;
@@ -41,12 +42,8 @@ WORD mem[128];     /* memoire                       */
 #define INST_HALT   (6)
 #define INST_SYSC   (7)
 #define INST_LOAD   (8)
-
-/**********************************************************
-** Codes associes aux instructions
-***********************************************************/
-
-
+#define INST_STORE  (9)
+#define INST_PUTMEM (10)
 
 /**********************************************************
 ** Placer une instruction en memoire
@@ -77,6 +74,7 @@ void make_inst(int adr, unsigned code, unsigned i, unsigned j, short arg) {
 #define SYSC_EXIT 			(100)
 #define SYSC_PUTI			 	(101)
 #define SYSC_NEW_THREAD	(102)
+#define SYSC_SLEEP      (103)
 
 /**********************************************************
 ** Le Mot d'Etat du Processeur (PSW)
@@ -99,6 +97,8 @@ typedef struct PSW {    /* Processor Status Word */
 
 #define EMPTY		(0)
 #define READY		(1)
+#define SLEEP   (2)
+#define GETCHAR (3)
 
 struct
 {
@@ -115,7 +115,7 @@ int current_process = -1;
 /* instruction d'addition */
 PSW cpu_ADD(PSW m) {
 	m.AC = m.DR[m.RI.i] += (m.DR[m.RI.j] + m.RI.ARG);
-	m.PC += 1;
+  m.PC += 1;
 	return m;
 }
 
@@ -133,6 +133,7 @@ PSW cpu_CMP(PSW m) {
 	return m;
 }
 
+/* instruction calcule d'allocation de taile */
 PSW cpu_LOAD( PSW m )
 {
 	m.AC = m.DR[ m.RI.j ] + m.RI.ARG;
@@ -150,9 +151,53 @@ PSW cpu_LOAD( PSW m )
 	return m;
 }
 
+/* instuction de stockage */
+PSW cpu_STORE( PSW m )
+{
+  m.AC = m.DR[ m.RI.j ] + m.RI.ARG;
+  if( m.AC < 0 || m.AC > m.SS )
+  {
+    m.IN = INT_SEGV;
+  }
+  else
+  {
+    mem[ m.SB + m.AC ] = m.DR[ m.RI.i ];
+    m.AC = m.DR[ m.RI.i ];
+    m.PC += 1;
+  }
+
+  return m;
+}
+
+/* instruction affichant une case memoire */
+PSW cpu_PUTMEM( PSW m )
+{
+  m.AC = m.DR[ m.RI.i ] + m.RI.ARG;
+  if( m.AC < 0 || m.AC >= m.SS )
+  {
+    m.IN = INT_SEGV;
+  }
+  else
+  {
+    m.AC = mem[m.SB + m.AC];
+    printf( "INST_PUTMEM : %d\n", m.AC );
+    m.PC += 1;
+  }
+
+  return m;
+}
+
 /* Simulation de la CPU */
 PSW cpu(PSW m) {
 	union { WORD word; INST in; } inst;
+
+  /*** on verifie si le thread ne s'est pas endormit ***/
+  if( process[ current_process ].state == SLEEP )
+  {
+    m.IN = INT_CLOCK;
+    cptInstrution = 0;
+    return m;
+  }
 
 	/*** lecture et decodage de l'instruction ***/
 	if (m.PC < 0 || m.PC >= m.SS) {
@@ -162,8 +207,8 @@ PSW cpu(PSW m) {
 	inst.word = mem[m.PC + m.SB];
 	m.RI = inst.in;
 	/*** execution de l'instruction ***/
-	switch (m.RI.OP) {
-    
+	switch (m.RI.OP) 
+  {  
     case INST_ADD:
       m = cpu_ADD(m);
       break;
@@ -200,20 +245,29 @@ PSW cpu(PSW m) {
     break;
 
     case INST_LOAD:
-			cpu_LOAD( m );
+			m = cpu_LOAD( m );
       break;
 
+    case INST_STORE:
+      m = cpu_STORE( m );
+      break;
 
+    case INST_PUTMEM:
+      m = cpu_PUTMEM( m );
+      break;
 
     default:
       /*** interruption instruction inconnue ***/
       m.IN = INT_INST;
-      return (m);
+      break;
 	}
 
 	/*** interruption apres chaque instruction ***/
-  if( (++cptInstrution & 0x3) == 0x3 && m.IN == INT_NONE )
+  if( ++cptInstrution >= 3 && m.IN == INT_NONE )
+  {
     m.IN = INT_CLOCK;
+    cptInstrution = 0;
+  }
 
   
   return m;
@@ -238,21 +292,50 @@ PSW systeme_init(void) {
 	// make_inst(2, INST_ADD, 0, 2, 200);   /* R0 += R2+200 */
 	// make_inst(3, INST_ADD, 0, 1, 100);   /* R0 += R1+100 */
 
-  make_inst( 0, INST_SUB, 1, 1, 0 );
-  make_inst( 1, INST_SUB, 2, 2, -1000 );
-  make_inst( 2, INST_SUB, 3, 3, -10 );
-  make_inst( 3, INST_SYSC, 1, 0, SYSC_PUTI );
-  make_inst( 4, INST_SYSC, 2, 0, SYSC_PUTI );
-  make_inst( 5, INST_SYSC, 3, 0, SYSC_PUTI );
-  make_inst( 6, INST_CMP, 1, 2, 0 );
-  make_inst( 7, INST_IFGT, 0, 0, 14 );
-  make_inst( 8, INST_NOP, 0, 0, 0 );
-  make_inst( 9, INST_NOP, 0, 0, 0 );
-  make_inst( 10, INST_NOP, 0, 0, 0 );
-  make_inst( 11, INST_ADD, 1, 3, 0 );
-  make_inst( 12, INST_SYSC, 1, 0, SYSC_PUTI );
-  make_inst( 13, INST_JUMP, 0, 0, 6 );
-  make_inst( 14, INST_HALT, 0, 0, 0 );
+  //make_inst( 0, INST_SUB, 1, 1, 0 );
+  //make_inst( 1, INST_SUB, 2, 2, -1000 );
+  //make_inst( 2, INST_SUB, 3, 3, -10 );
+  //make_inst( 3, INST_SYSC, 1, 0, SYSC_PUTI );
+  //make_inst( 4, INST_SYSC, 2, 0, SYSC_PUTI );
+  //make_inst( 5, INST_SYSC, 3, 0, SYSC_PUTI );
+  //make_inst( 6, INST_CMP, 1, 2, 0 );
+  //make_inst( 7, INST_IFGT, 0, 0, 14 );
+  //make_inst( 8, INST_NOP, 0, 0, 0 );
+  //make_inst( 9, INST_NOP, 0, 0, 0 );
+  //make_inst( 10, INST_NOP, 0, 0, 0 );
+  //make_inst( 11, INST_ADD, 1, 3, 0 );
+  //make_inst( 12, INST_SYSC, 1, 0, SYSC_PUTI );
+  //make_inst( 13, INST_JUMP, 0, 0, 6 );
+  //make_inst( 14, INST_HALT, 0, 0, 0 );
+
+  //make_inst( 0, INST_SUB, 1, 1, 0 );      // R1 : valeur = 0
+  //make_inst( 1, INST_SUB, 2, 2, 0 );      // R2 : Case memoir = 0
+  //make_inst( 2, INST_ADD, 2, 2, 0 );      // R2 = 0
+  //make_inst( 3, INST_SYSC, 0, 0, SYSC_NEW_THREAD ); //Création nouveau thread
+  //make_inst( 4, INST_IFGT, 0, 0, 8);      // Test père/fils
+  ///* --- FILS --- */
+  //make_inst( 5, INST_ADD, 1, 2, 1 );      // R1 = R1 + 1
+  //make_inst( 6, INST_STORE, 1, 2, 15 );   // Stocker R1 dans case de R2 + 15 ( = 15)
+  //make_inst( 7, INST_JUMP, 0, 0, 5);      // PC = 5
+  ///* --- PERE --- */
+  //make_inst( 8, INST_PUTMEM, 2, 0, 15 );  // Affiche la case mémoire R2 + 15 ( = 15)
+  //make_inst( 9, INST_NOP, 0, 0, 0 );      // NOP pour avoir trois instruction et un seul affichage console
+  //make_inst( 10, INST_JUMP, 0, 0, 8 );     // PC = 8
+
+  //make_inst( 1, INST_SYSC, 0, 0, SYSC_NEW_THREAD ); //Nouveau thread
+  //make_inst( 2, INST_IFGT, 0, 0, 8 ); // Test Pere/Fils
+  ///* --- FILS --- */
+  //make_inst( 3, INST_SUB, 1, 1, 0 );    // R1 = 0
+  //make_inst( 4, INST_ADD, 1, 1, 3 );    // R1 = 3
+  //make_inst( 6, INST_SYSC, 1, 0, SYSC_PUTI );
+  //make_inst( 5, INST_SYSC, 1, 0, SYSC_SLEEP );
+  //make_inst( 7, INST_JUMP, 0, 0, 3 );
+  ///* --- PERE --- */
+  //make_inst( 8, INST_SUB, 1, 1, 0 );    // R2 = 0
+  //make_inst( 9, INST_ADD, 1, 1, 1 );    // R2 = 1
+  //make_inst( 11, INST_SYSC, 1, 0, SYSC_PUTI );
+  //make_inst( 10, INST_SYSC, 1, 0, SYSC_SLEEP );
+  //make_inst( 12, INST_JUMP, 0, 0, 8 );
 
 
 	/*** valeur initiale du PSW ***/
@@ -263,8 +346,8 @@ PSW systeme_init(void) {
 
 	process[0].cpu = cpu;
 	process[0].state = READY;
-	process[1].cpu = cpu;
-	process[1].state = READY;
+	//process[1].cpu = cpu;
+	//process[1].state = READY;
 	current_process = 0;
 
 	return cpu;
@@ -281,7 +364,7 @@ PSW systeme(PSW m) {
 			return (systeme_init());
 
 		case INT_SEGV:
-      printf( "%d - INT_SEGV received...\n", m.IN );
+      printf( "%d - INT_SEGV received (instruction : %d)\n", m.IN, m.PC );
       exit( INT_SEGV );
 			break;
 		
@@ -292,50 +375,67 @@ PSW systeme(PSW m) {
 			break;
 		
     case INT_INST:
-      printf( "%d - INT_INST received...\n", m.IN );
+      printf( "%d - INT_INST received (instruction : %d)\n", m.IN, m.PC );
       exit( INT_INST );
 			break;
 
 
     case INT_CLOCK:
 			if( current_process != -1 )
-			{
 				process[ current_process ].cpu = m;
-				process[ current_process ].state = READY;
-			}
 
 			do
 			{
 				current_process = (current_process + 1) % MAX_PROCESS;
+
+        if( process[ current_process ].state == SLEEP && time(NULL) >= process[current_process].cpu.AC )
+          process[current_process].state = READY;
 			}
 			while( process[ current_process ].state != READY );
-			
-			return process[ current_process ].cpu;
+		
+			m = process[ current_process ].cpu;
       break;
 
-    // *** SYSC interruption
+    // *** SYSC interruption ***
 		case SYSC_PUTI:
-			printf( "SYSC_PUTI : %d\n", m.DR[ m.RI.i ] );
+			printf( "SYSC_PUTI (proc : %d - i : %d) : %d\n", current_process, m.RI.i, m.DR[ m.RI.i ] );
 			break;
 
     case SYSC_EXIT:
       exit( 0 );
 
 		case SYSC_NEW_THREAD:
-			int tmpId = -1;
+    {
+			int filsId = 0;
 			do
 			{
-				tmpId = current_process + 1 % MAX_PROCESS;
+        filsId = ( filsId + 1 ) % MAX_PROCESS; 
 			}
-			while( process[ tmpId ].state = EMPTY );
-			m.DR
+			while( process[ filsId ].state = EMPTY );
+      
+      m.DR[ m.RI.i ] = filsId;			
+      m.AC = filsId;
+      
+      process[ filsId ].cpu = m;
+      process[ filsId ].state = READY;
+      process[ filsId ].cpu.DR[ m.RI.i ] = 0;
+      process[ filsId ].cpu.AC = 0;
+      process[ filsId ].cpu.PC = m.PC + 1;
+
+      break;
+    }
+
+    case SYSC_SLEEP:
+      process[ current_process ].state = SLEEP;
+      m.AC = m.DR[ m.RI.i ] + time(NULL);  // AC = time() + Ri (Date de reveil)
+      break;
 
 		default:
 			break;
 	}
 
 
-  //system( "sleep 0.05" );
+  system( "sleep 0.05" );
   m.IN = INT_NONE;
 	return m;
 }
